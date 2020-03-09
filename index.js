1bb0aaf12bcbaf9773fe7309d7f1a6ed8bbd0e73
Su Anli <sual0001@gmail.com>
2020-03-09 15:31:50 +0800

    feat(home-screen): add react-navigation lib, basic home screen ui

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+import {App} from '@components';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import {App} from './src/components';
 
 AppRegistry.registerComponent(appName, () => App);
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,23 @@
+import {HomeScreen} from '@components';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import 'react-native-gesture-handler';
+
+const Stack = createStackNavigator();
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>App</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <HomeStack />
+    </NavigationContainer>
   );
 };
 
